f4febb97959cd59e5308af2280a8f3e09887bad5
Askarbek Akhrorov <151575476+Asqarusa@users.noreply.github.com>
2024-11-03 21:50:41 -0800

    Initial commit

---
+++ Moshphyton.py
@@ -0,0 +1,44 @@
+# print("hello world")
+
+# age = 20
+# age = 30
+# price = 19.95
+# first_name = "Asqar"
+# is_online = False 
+# print(age)
+# Name = input("What's your name ?")
+# Age = input("what's your  age ?")
+# Patient = input("Are you new  patient ?")
+
+# print('New patient is' + Name , 'He is' + Age 'years old', "he  is a new patient")
+
+
+# first= (input('enter first number '))
+# second = (input('enter second number '))
+# sum = float(first) +  float(second)
+
+# print('Sum: '+ str(sum))
+
+# course = "First time Phyton"
+# # print(course.replace("n", "m")) #lower-upper-add-replace-find
+# print("time" in course)
+
+# changing string never  change the original string it returns  a new string.
+
+
+# Arithmetic Operations
+# print(10 // 3) #butun son
+# print(10 / 3)
+# print(10 % 3) #qoldiq
+# print(10 ** 3) #10*10*10
+
+# x=10
+# x = x + 3
+# x+=5
+# x-=9
+# print(x)
+
+
+
+
+
